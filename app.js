18d5286f715458103c4bfce2a39e001497356875
Cristian Fitata <cristianfitata@ustadistancia.edu.co>
2022-10-05 15:42:11 -0500

    settings to deploy

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 
 // Routers
 const { usersRouter } = require("./routes/users.routes");
@@ -7,9 +13,20 @@ const { cartsRouter } = require("./routes/cart.routes");
 
 // Controllers
 const { globalErrorHandler } = require("./controllers/error.controller");
+const { AppError } = require("./utils/appError.util");
 
 // Init our Express app
 const app = express();
+
+//USING DEPLOY DEPENDENCIES...
+
+//Addin security headers...
+app.use(helmet());
+//Compressing responses...
+app.use(compression());
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+else if (process.env.NODE_ENV === "production") app.use(morgan("combined"));
 
 // Enable Express app to receive JSON data
 app.use(express.json());
